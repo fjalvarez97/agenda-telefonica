84a6b3217d9ea939bbd03561f871722620039f52
Fran Alvarez <franjalvarez955@gmail.com>
2018-02-15 20:20:35 +0100

    Primer Commit

---
+++ AgendaTelefonica.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void enterNumber(String name, String number)
+    {
+        
+    }
+    
+    public String lookupNumber(String name)
+    {
+        return "";
+    }
+}
